9d1ac0252b66f03bc951d944ca99a00395c59825
UsrNameu1 <tea_room_1028@yahoo.co.jp>
2015-02-13 02:22:40 +0900

    Initial Commit

---
+++ QuickStudy/QuickStudy.h
@@ -0,0 +1,19 @@
+//
+//  QuickStudy.h
+//  QuickStudy
+//
+//  Created by adachi yuichi on 2015/02/13.
+//  Copyright (c) 2015年 yad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for QuickStudy.
+FOUNDATION_EXPORT double QuickStudyVersionNumber;
+
+//! Project version string for QuickStudy.
+FOUNDATION_EXPORT const unsigned char QuickStudyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <QuickStudy/PublicHeader.h>
+
+
